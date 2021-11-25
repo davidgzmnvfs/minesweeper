5c77b5f68cfb7d78cdf3e247e7d2bfc3874e4fc1
davidgzmnvfs <dguzman@vfs.com>
2021-11-25 15:33:44 -0800

    moved the event listener to a single one at table level, fixed bugged ids

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,7 @@ class Cell {
         this.hasMine = false;
         this.flagged = false;
         this.neighborMines = 0;
-    }
-    Reveal( ){
-    }
-    FlagMine(){
+        this.revealed = false;
     }
 }
 
@@ -41,7 +38,7 @@ class Grid {
     Draw(){
         for(let i = 0; i < this.size ; i++){
             for (let j = 0; j < this.size ; j++){
-                let idString = `#cell${i}${j}`;
+                let idString = `#cell${i}cell${j}`;
                 let currentCell = document.querySelector(idString);
                 currentCell.classList.add(this.cells[i][j].hasMine ? "mine" : "safe");
             }
@@ -61,37 +58,91 @@ class Grid {
         }
         console.log(printingLine);
     }
-    HandleClick(x,y){
-        console.log(this.cells[x][y].hasMine ? 'mine' : 'no mine');
-    }
-    SetClick(){
+    Lose(){
+        console.log("You lost");
         for(let i = 0; i < this.size ; i++){
             for (let j = 0; j < this.size ; j++){
-                let idString = `#cell${i}${j}`;
-                let currentCell = document.querySelector(idString);
-                currentCell.addEventListener('click', event => {
-                    // console.log(`Event listener added to ${i},${j} `)
-                    this.HandleClick(i,j);
-                });
+                let cell =this.cells[i][j];
+                if(cell.hasMine){
+                    this.Reveal(cell);
+                }
             }
         }
+        }
+    HandleClick(x,y){
+        console.log(this.cells[x][y].hasMine ? 'mine' : 'no mine');
+        const cell = this.cells[x][y];
+        if(cell.hasMine){
+            this.Lose();
+        }else{
+            this.Reveal(cell);
+        }
+    }
+    SetClick(){
+        document.querySelector("#table").addEventListener('click', event => {
+            let x = Math.floor(event.target.dataset.row);
+            let y = Math.floor(event.target.dataset.col);
+            this.HandleClick(x,y);
+            console.log(x);
+        });
+        // for(let i = 0; i < this.size ; i++){
+        //     for (let j = 0; j < this.size ; j++){
+        //         let idString = `#cell${i}cell${j}`;
+        //         let currentCell = document.querySelector(idString);
+        //         currentCell.addEventListener('click', event => {
+        //             let x = Math.floor(event.target.getAttribute("data-row"));
+        //             this.HandleClick(i,j);
+        //         });
+        //     }
+        // }
     }
     GenerateHtml(){
-        let htmlString = "<table>"
+        const container = document.querySelector(".container");
+        let htmlString = "<table id=\"table\">"
         for(let i = 0 ; i < this.size ; i++){
             htmlString += "<tr>";
             for(let j = 0 ; j < this.size ; j++){
-                htmlString += `<td class="cell" id="cell${i}${j}">`;
+                htmlString += `<td class="cell" id="cell${i}cell${j}" data-row="${i}" data-col="${j}"></td>`;
             }
             htmlString += "</tr>";
-            
         }
         htmlString += "</table>"
+        container.innerHTML = htmlString;
+    }
+
+    Reveal(cell){
+        setTimeout(() => {
+            if(!cell.revealed){
+                console.log(`revealing cell ${cell.xPos}${cell.yPos}`)
+                cell.revealed = true;
+                document.querySelector(`#cell${cell.xPos}cell${cell.yPos}`).classList.add("revealed");
+                
+                let nextX,nextY;
+                for(let i = -1 ; i <= 1 ; i++){
+                    nextX = cell.xPos + i;
+                    for(let j = -1 ; j <= 1 ; j++){
+                        nextY = cell.yPos + j;
+                        if (
+                            nextY + j > -1 &&
+                            nextY + j < this.size&&
+                            nextX > -1 &&
+                            nextX < this.size
+                            ){
+                                let nextCell = this.cells[nextX][nextY];
+                                if(!nextCell.hasMine){
+                                    this.Reveal(nextCell);
+                                }
+                        }
+                    }
+                }
+            }
+        }, 50);
     }
 }
 
-let myGrid = new Grid(5,10);
+let myGrid = new Grid(20,5);
 myGrid.Generate();
+myGrid.GenerateHtml();
 myGrid.SetMines();
 myGrid.SetClick();
 myGrid.Print();
